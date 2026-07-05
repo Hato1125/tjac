336e9c1a76074f5e704290de28f7548493b78777
Hato1125 <hato.flavius@gmail.com>
2026-07-05 15:10:14 +0900

    chore: define test/playground targets only when present

--- xmake.lua
+++ xmake.lua
@@ -17,10 +17,24 @@ target('libtjac')
   add_files('src/*.cc')
   add_includedirs('src')
 
+-- test/ と playground/ はローカル専用 (git管理外) なので、ある場合だけ定義する
+if os.isdir(path.join(os.scriptdir(), 'test')) then
+  target('test')
+    set_kind('binary')
+    set_default(false)
+    add_files('test/*.cc')
+    add_includedirs('src')
+    add_deps('libtjac')
+    add_tests('default')
+end
+
 -- 雑デバッグ用の遊び場。中身は自由に書き換えてよい
-target('playground')
-  set_kind('binary')
-  set_default(false)
-  add_files('playground/*.cc')
-  add_includedirs('src')
-  add_deps('libtjac')
+if os.isdir(path.join(os.scriptdir(), 'playground')) then
+  target('playground')
+    set_kind('binary')
+    set_default(false)
+    add_files('playground/*.cc')
+    add_includedirs('src')
+    add_deps('libtjac')
+    add_links('SDL3')
+end
